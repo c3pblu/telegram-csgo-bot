8c04bf8085ac723e8f6844bdae1bd3e00d78a3c6
Siarhei Yurkin <Siarhei.Yurkin@standardbank.co.za>
2019-11-07 21:39:20 +0300

    menu update, small fixes

--- README.md
+++ README.md
@@ -3,11 +3,12 @@ Get data from [HLTV.org](https://HLTV.org/) about CS:GO events, teams, players e
 
 ### Done:
 - Top teams and players
+- Matches
+- Results
 - Menu
 - Randomize private bot response
 - Skip messages after 5 minutes gone
 
 ### To-Do:
-- Matches
-- Results
+
 - Flags
--- src/main/java/com/telegram/bot/csgo/model/MenuMessage.java
+++ src/main/java/com/telegram/bot/csgo/model/MenuMessage.java
@@ -7,6 +7,8 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 
+import com.vdurmont.emoji.EmojiParser;
+
 public class MenuMessage extends SendMessage {
 	
 	private static final InlineKeyboardMarkup MARK_UP_IN_LINE = new InlineKeyboardMarkup();
@@ -16,14 +18,14 @@ public class MenuMessage extends SendMessage {
 	private static final List<InlineKeyboardButton> ROW_3 = new ArrayList<>();
 	
 	static {
-		ROW_1.add(new InlineKeyboardButton().setText("Матчи").setCallbackData(Constants.MATCHES));
-		ROW_1.add(new InlineKeyboardButton().setText("Результаты").setCallbackData(Constants.RESULTS));
-		ROW_2.add(new InlineKeyboardButton().setText("Топ 10").setCallbackData(Constants.TOP_10));
-		ROW_2.add(new InlineKeyboardButton().setText("Топ 20").setCallbackData(Constants.TOP_20));
-		ROW_2.add(new InlineKeyboardButton().setText("Топ 30").setCallbackData(Constants.TOP_30));
-		ROW_3.add(new InlineKeyboardButton().setText("Топ 10 Игроков").setCallbackData(Constants.TOP_10_PLAYERS));
-		ROW_3.add(new InlineKeyboardButton().setText("Топ 20 Игроков").setCallbackData(Constants.TOP_20_PLAYERS));
-		ROW_3.add(new InlineKeyboardButton().setText("Топ 30 Игроков").setCallbackData(Constants.TOP_30_PLAYERS));
+		ROW_1.add(new InlineKeyboardButton().setText("\uD83D\uDD25 Матчи").setCallbackData(Constants.MATCHES));
+		ROW_1.add(new InlineKeyboardButton().setText("\uD83C\uDFC6 Результаты").setCallbackData(Constants.RESULTS));
+		ROW_2.add(new InlineKeyboardButton().setText(EmojiParser.parseToUnicode(":military_medal:") + "Топ 10").setCallbackData(Constants.TOP_10));
+		ROW_2.add(new InlineKeyboardButton().setText(EmojiParser.parseToUnicode(":military_medal:") + "Топ 20").setCallbackData(Constants.TOP_20));
+		ROW_2.add(new InlineKeyboardButton().setText(EmojiParser.parseToUnicode(":military_medal:") + "Топ 30").setCallbackData(Constants.TOP_30));
+		ROW_3.add(new InlineKeyboardButton().setText(EmojiParser.parseToUnicode(":sports_medal:") + "Топ 10 Игроков").setCallbackData(Constants.TOP_10_PLAYERS));
+		ROW_3.add(new InlineKeyboardButton().setText(EmojiParser.parseToUnicode(":sports_medal:") + "Топ 20 Игроков").setCallbackData(Constants.TOP_20_PLAYERS));
+		ROW_3.add(new InlineKeyboardButton().setText(EmojiParser.parseToUnicode(":sports_medal:") + "Топ 30 Игроков").setCallbackData(Constants.TOP_30_PLAYERS));
 		ROWS_IN_LINE.add(ROW_1);
 		ROWS_IN_LINE.add(ROW_2);
 		ROWS_IN_LINE.add(ROW_3);
